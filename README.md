8ec213ff7ad672f68b365717c653fd8687933fea
samuelsbftw <135280378+samuelsbftw@users.noreply.github.com>
2023-06-02 17:25:02 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Abra esta pagina en [https://samuelsbftw.github.io/frase/](https://samuelsbftw.github.io/frase/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/samuelsbftw/frase** e importar
+
+## Edita este proyecto ![Build status badge](https://github.com/samuelsbftw/frase/workflows/MakeCode/badge.svg)
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/samuelsbftw/frase** y haga clic en importar
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/samuelsbftw/frase/raw/master/.github/makecode/blocks.png)
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,91 @@
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.forever(function () {
+	
+})
